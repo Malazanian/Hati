e20e25b3f427167a8d0f8da567d4e688450531b8
Ian Rutner <irutner22@gmail.com>
2020-03-29 16:15:14 -0500

    update functionality for server removal

---
+++ clean.js
@@ -0,0 +1,8 @@
+module.exports = {
+    clean(text) {
+        if (typeof(text) === "string")
+            return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+        else
+            return text;
+    }
+}
